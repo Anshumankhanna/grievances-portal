334b531eb466f45227a431983d4185a1fa980978
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2024-11-17 13:01:09 +0530

    Fixed the Background.tsx component
    - Background component wasn't displaying its child elements correctly, set their properties so that wouldn't be an issue anymore
    - Added a new utility to `tailwind.config.ts`: `.absolute-center`
    - Changed how the images are displayed in the background, instead of setting an interval function, utilised `useEffect` and `setTimeout` to create an animation that works more safely.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Page() {
 	return (
 		<div>
-			
+			All new components to be added here <br />
+			This is src/app/page.tsx
 		</div>
 	);
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,8 +26,14 @@ export default {
 			addUtilities({
 				".full": {
 					"width": "100%",
-					"height": "100%"
-				}
+					"height": "100%",
+				},
+				".absolute-center": {
+					"position": "absolute",
+					"top": "50%",
+					"left": "50%",
+					"transform": "translate(-50%, -50%)",
+				},
 			});
 		})
 	],
