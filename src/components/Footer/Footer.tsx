d86a79f86f536cb7919213e069d27614f849f210
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-02-08 20:51:37 +0530

    Updated Footer, added a semicolon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,5 +3,5 @@ export default function Footer() {
         <div className="w-full h-10 flex justify-center items-center text-center text-gray-600 bg-foreground">
             <a href="https://www.linkedin.com/in/anshuman-khanna-02553b22a/" className="hover:underline" target="_blank">This site is created by Anshuman Khanna</a>
         </div>
-    )
+    );
 };
