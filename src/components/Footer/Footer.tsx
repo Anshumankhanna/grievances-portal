fb9ac719390baa3d281f1ffb7e762240051fe054
user.name like: "Anshuman Khanna <user.email like: "anshumankh01@gmail.com>
2025-02-08 20:38:25 +0530

    Made footer a link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="w-full h-10 flex justify-center items-center text-center text-gray-600 bg-foreground">
-            This site is created by Anshuman Khanna
+            <a href="https://www.linkedin.com/in/anshuman-khanna-02553b22a/" className="hover:underline" target="_blank">This site is created by Anshuman Khanna</a>
         </div>
     )
 };
